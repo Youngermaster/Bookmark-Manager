0b131ec9de4f0e74f2c817dba0dcdb90ba0cd89d
Youngermaster <jmyoungh@eafit.edu.co>
2019-07-11 14:10:50 -0500

    Initial commit

---
+++ lib/app_component.dart
@@ -0,0 +1,13 @@
+import 'package:angular/angular.dart';
+
+import 'src/bm_scaffold/bm_scaffold_component.dart';
+
+@Component(
+  selector: 'my-app',
+  styleUrls: ['app_component.css'],
+  templateUrl: 'app_component.html',
+  directives: [BookmarkScaffoldComponent]
+)
+class AppComponent {
+  // Nothing here yet. All logic is in TodoListComponent.
+}
